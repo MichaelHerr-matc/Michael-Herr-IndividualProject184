3ea122f4ad1adacc8cafcad87c387e16eea9ca55
Michael Her <herrml@gmatc.matc.edu>
2019-11-30 15:34:47 -0600

    Initial Commit

---
+++ IndividualProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IndividualProject
+//
+//  Created by Michael Her on 11/30/19.
+//  Copyright © 2019 Michael Her. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
